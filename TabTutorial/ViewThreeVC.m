9aa651ac1453df166273ff6a5b4a9e807ff72aae
Wa-jiw Casey <wajiw.casey@gmail.com>
2011-08-14 14:59:36 -0500

    committing all files

---
+++ TabTutorial/ViewThreeVC.m
@@ -0,0 +1,58 @@
+//
+//  ViewThreeVC.m
+//  TabTutorial
+//
+//  Created by Wa-jiw Casey on 8/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewThreeVC.h"
+
+
+@implementation ViewThreeVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    //self.tabBarItem.badgeValue = @"badge";
+    self.tabBarItem.image = [UIImage imageNamed:@"arrow.png"];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
